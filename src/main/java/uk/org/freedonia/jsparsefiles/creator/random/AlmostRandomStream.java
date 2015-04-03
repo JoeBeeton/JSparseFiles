7a5533c241ad21de517b1d566799da3162297f5a
Joe Beeton <jbeeton@interica.com>
2015-04-03 23:49:07 +0100

    added new ( almost ) random InputStream which generats a almost random stream of data as well as a unit test to verify this.
    Signed-off-by: Joe Beeton <jbeeton@interica.com>

---
+++ src/main/java/uk/org/freedonia/jsparsefiles/creator/random/AlmostRandomStream.java
@@ -0,0 +1,44 @@
+package uk.org.freedonia.jsparsefiles.creator.random;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+
+import org.apache.commons.lang3.RandomUtils;
+
+
+/**
+ * Returns an almost random stream of bytes forever.
+ * However to help denote the start and end of the sparseness in the file I've
+ * excluded the NULL ( zero ) bytes from the stream. All Zeros are now 1's.
+ * @author jbeeton
+ *
+ */
+public class AlmostRandomStream extends InputStream {
+
+	LinkedList<Byte> almostRandomBuffer = new LinkedList<Byte>();
+	
+	
+	@Override
+	public int read() throws IOException {
+		
+		if ( almostRandomBuffer.isEmpty() ) {
+			loadNewRandomyData();
+		}
+		return almostRandomBuffer.removeFirst();
+	}
+	
+	/**
+	 * loads a new random array of 4096 bytes into the linkedlist.
+	 */
+	private void loadNewRandomyData() {
+		byte[] byteArray = RandomUtils.nextBytes(4096);
+		for (int i = 0; i < byteArray.length; i++) {
+			if ( byteArray[i] == (byte)0 ) {
+				byteArray[i] = (byte)1;
+			}
+			almostRandomBuffer.add(byteArray[i]);
+		}
+	}
+
+}
---
+++ src/test/java/uk/org/freedonia/jsparsefiles/creator/random/TestAlmostRandomStream.java
@@ -0,0 +1,23 @@
+package uk.org.freedonia.jsparsefiles.creator.random;
+
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+public class TestAlmostRandomStream {
+	
+	@Test
+	public void testThatNoZeroBytesAreInStream() throws IOException {
+		try( InputStream randomStream = new AlmostRandomStream() ) {
+			for ( int i = 0; i < FileUtils.ONE_MB; i++ ) {
+				assertFalse( "Found 0 byte at position : " + i,
+						randomStream.read() == 0 );
+			}
+		}
+	}
+
+}
